9863131346f72c9747d5df88b023cadc4eb4d7ab
kyle <macadmin@MacVicdeMacBook-Pro.local>
2020-03-25 09:07:23 +0800

    Added Update Tasks and Updated Streaks

--- index.js
+++ index.js
@@ -13,17 +13,18 @@ firebase.initializeApp(firebaseConfig);
 
 // Global Variables
 var tasks = [];
+var streaks = 0;
+var identifier = 0;
 
 //Writing data into arrays
 function writeData(){
   //Saving data into the array
   var taskName = document.getElementById('nameField').value;
   var taskDate = document.getElementById('dateField').value;
-  var newTask = [taskName, taskDate];
-  tasks.push(newTask);
 
-  //index of the newest task
-  var index = tasks.length - 1;
+  var newTask = [taskName, taskDate, identifier];
+
+  tasks.push(newTask);
 
   //Getting the Date assigned
   var dateAssigned = new Date();
@@ -39,28 +40,49 @@ function writeData(){
     var div = document.createElement("div");
 
     //Editing the inner part of the Div
-    div.innerHTML = taskName + "<br>Assigned on " + dateAssigned + "<br>Due On " + taskDate + "<br>" + "<button class = 'button' id = " + index +" onclick = 'deleteData(this.id)'>Delete</button>";
+    div.innerHTML = taskName + "<br>Assigned on " + dateAssigned + "<br>Due On " + taskDate + "<br>" + "<button class = 'button remove' id =" + identifier +" onclick = 'deleteData(this.id)'>Done ✔</button>";
+
+    div.classList.add("task" + identifier);
+
+    identifier++;
 
     //Giving the Div a class name
-    var className = "task" + index;
-    var className2 = "task";
-    div.classList.add(className);
     div.classList.add("task");
+
 
 
   //Printing the array into the HTML website
   document.querySelector(".displayArea").appendChild(div);
+
+  //Updating the Tasks
+  document.querySelector(".taskLeft").innerHTML = "Tasks Left : " + tasks.length;
 }
 
-function deleteData(index){
+
+function deleteData(id){
+  console.log(id);
   //Removing value from the array
-  tasks.splice(index,1);
+  for(var i = 0; i < tasks.length-1; i++){
+    task = tasks[i];
+    if(task[2] == id){
+      tasks.splice(i,1);
+    }
+  }
+
   //Creating the div name to be deleted
-  var dName = ".task"+index;
+  var dName = ".task" + id;
   var e = document.querySelector(dName);
   e.parentNode.removeChild(e);
 
+  //Add to the streaks
+  streaks++;
+  document.querySelector(".streaks").innerHTML= "<h1 style = 'font-size: 30px; margin-bottom: -15px;'>🔥</h1><br> Streaks : " + streaks;
+
+  //Updating the Tasks
+  document.querySelector(".taskLeft").innerHTML = "Tasks Left : " + tasks.length;
+  console.log(tasks)
 }
+
 
 function searchTask(){
   var searchTerm = document.querySelector(".search").value;
@@ -69,13 +91,24 @@ function searchTask(){
   //Clearing the innerHTML of the display area to prevent reprinting
   document.querySelector(".searchResult").innerHTML = "";
 
+  //Create Div
+
+  var div = document.createElement("div");
+  div.classList.add("searchTask");
+
 
   for(var i = 0; i < tasks.length; i++){
     var task = tasks[i];
     //If the name of the task (in lower case) contains the search term
     //We print it into results section
     if(task[0].toLowerCase().includes(searchTerm)){
-      document.querySelector(".searchResult").innerHTML += "<h3>" + task[0] +  "<br>Due On " + task[1] +"</h3>";
+
+      div.innerHTML = task[0] + "<br>Due On " + task[1];
+
+      document.querySelector(".searchResult").appendChild(div);
+
+
+      // document.querySelector(".searchResult").innerHTML += "<h3>" + task[0] +  "<br>Due On " + task[1] +"</h3>";
     }
   }
 
@@ -112,6 +145,7 @@ function login(){
 function logout(){
   firebase.auth().signOut();
 }
+
 
 // W3 Schools Side Bar Functions
 
